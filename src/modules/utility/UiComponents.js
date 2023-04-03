e40bd8cb352b9dfa8b8dd848efed69d157e84ed1
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2023-04-03 12:59:53 +0300

    update modules to latest

--- src/modules/utility/UiComponents.js
+++ src/modules/utility/UiComponents.js
@@ -1,42 +1,57 @@
-import Button from 'react-bootstrap/lib/Button';
-import DropdownButton from 'react-bootstrap/lib/DropdownButton';
-import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
-import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
-import Table from 'react-bootstrap/lib/Table';
-import Col from 'react-bootstrap/lib/Col';
-import Row from 'react-bootstrap/lib/Row';
-import Alert from 'react-bootstrap/lib/Alert';
-import Form from 'react-bootstrap/lib/Form';
-import Image from 'react-bootstrap/lib/Image';
-import Modal from 'react-bootstrap/lib/Modal';
-import Accordion from 'react-bootstrap/lib/Accordion';
-import Panel from 'react-bootstrap/lib/Panel';
-import PanelGroup from 'react-bootstrap/lib/PanelGroup';
-import Jumbotron from 'react-bootstrap/lib/Jumbotron';
-import Nav from 'react-bootstrap/lib/Nav';
-import Navbar from 'react-bootstrap/lib/Navbar';
-import NavItem from 'react-bootstrap/lib/NavItem';
-import FormControl from 'react-bootstrap/lib/FormControl';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
-import InputGroup from 'react-bootstrap/lib/InputGroup';
-import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
-import Popover from 'react-bootstrap/lib/Popover';
-import ProgressBar from 'react-bootstrap/lib/ProgressBar';
-import MenuItem from 'react-bootstrap/lib/MenuItem';
-import Collapse from 'react-bootstrap/lib/Collapse';
-import Tabs from 'react-bootstrap/lib/Tabs';
-import Tab from 'react-bootstrap/lib/Tab';
-import Label from 'react-bootstrap/lib/Label';
-import ListGroup from 'react-bootstrap/lib/ListGroup';
-import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
-import Pagination from 'react-bootstrap/lib/Pagination';
-import Well from 'react-bootstrap/lib/Well';
-import Tooltip from 'react-bootstrap/lib/Tooltip';
-import HelpBlock from 'react-bootstrap/lib/HelpBlock';
-import Checkbox from 'react-bootstrap/lib/Checkbox';
-import ControlLabel from 'react-bootstrap/lib/ControlLabel';
-import Overlay from 'react-bootstrap/lib/Overlay';
-import SplitButton from 'react-bootstrap/lib/SplitButton';
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Table from 'react-bootstrap/Table';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Alert from 'react-bootstrap/Alert';
+import Form from 'react-bootstrap/Form';
+import Image from 'react-bootstrap/Image';
+import Modal from 'react-bootstrap/Modal';
+import Accordion from 'react-bootstrap/Accordion';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavItem from 'react-bootstrap/NavItem';
+import FormControl from 'react-bootstrap/FormControl';
+import FormGroup from 'react-bootstrap/FormGroup';
+import InputGroup from 'react-bootstrap/InputGroup';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Collapse from 'react-bootstrap/Collapse';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import Pagination from 'react-bootstrap/Pagination';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Overlay from 'react-bootstrap/Overlay';
+import SplitButton from 'react-bootstrap/SplitButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+function Jumbotron({ children }) {
+  return <div className="jumbotron">{children}</div>;
+}
+
+function Checkbox(props) {
+  return <Form.Check type="checkbox" {...props} />;
+}
+
+function Label(props) {
+  return <Form.Label {...props} />;
+}
+
+function Well({ children }) {
+  return <div className="well">{children}</div>;
+}
+
+const MenuItem = Dropdown.Item;
+const ControlLabel = Form.Label;
+const HelpBlock = Form.Text;
 
 export {
   Col,
@@ -51,8 +66,6 @@ export {
   Image,
   Modal,
   Accordion,
-  Panel,
-  PanelGroup,
   Jumbotron,
   Nav,
   Navbar,
